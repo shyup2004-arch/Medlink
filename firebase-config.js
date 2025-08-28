795315025a8c34ce3503927a7eaee9821acba34d
shyup2004-arch <shyup2004@gmail.com>
2025-08-29 03:07:03 +0900

    Firebase 연동 설정

---
+++ firebase-config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSRpBU1egGq9aMK0LeBYq5cIFUkKEerco",
+  authDomain: "medlink-a8a75.firebaseapp.com",
+  projectId: "medlink-a8a75",
+  storageBucket: "medlink-a8a75.firebasestorage.app",
+  messagingSenderId: "918056247126",
+  appId: "1:918056247126:web:960ff46334003630bb40f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
